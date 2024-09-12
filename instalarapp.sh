a07f3589c4abd763b8f65282df4a04d13f53d5ca
Meenn <meenn@melacorneta.cl>
2024-09-12 00:01:49 -0300

    simple

--- instalarapp.sh
+++ instalarapp.sh
@@ -4,21 +4,8 @@
 echo "Actualizando lista de paquetes..."
 sudo apt update
 
-# Lista de programas a instalar
-programas=(
-  "htop"
-  "vim"
-  "wget"
-  "snmpd"
-  "fail2ban"
-)
-
 # Instalar los programas
-echo "Instalando programas..."
-for programa in "${programas[@]}"; do
-  echo "Instalando $programa..."
-  sudo apt install -y "$programa"
-done
+sudo apt install -y vim htop fail2ban
 
 echo "Limpieza de paquetes innecesarios..."
 sudo apt autoremove -y
