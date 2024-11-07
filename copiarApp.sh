5bdc578985ab16ab2e6a577daf54d6d2e062926a
Meenn-Windows <meenn@melacorneta.cl>
2024-11-07 20:46:59 -0300

    se corrige copiar

--- copiarApp.sh
+++ copiarApp.sh
@@ -2,20 +2,31 @@
 #
 #Copiado de SNMP
 cp ./snmpd/* /etc/snmp/snmpd.conf.d/
+echo
 echo "Copiado de SNMP de forma correcta..."
+echo
 #
 #Copiado de Fail2ban
 cp ./fail2ban/* /etc/fail2ban/
+echo
 echo "Copiado de JAIL de forma correcta..."
+echo
 #
 #Copiado de Aplicaciones de UFW
 cp ./ufw/* /etc/ufw/applications.d/
+echo
 echo "Copiado de Aplicaciones UFW de forma correcta..."
+echo
 #
 #Copiado de SSH
-cp .ssh/ssh-servidor.conf /etc/ssh/sshd_config.d/
-cp .ssh/banner /etc/ssh/
+cp ./ssh/ssh-servidor.conf /etc/ssh/sshd_config.d/
+cp ./ssh/banner /etc/ssh/
+echo
 echo "Copiado configuracion de SSH de forma correcta..."
+echo
 #
 #Copiando nginx de sitio web
-cp ./nginx/* /var/www/html
+cp -r ./nginx/* /var/www/html
+echo
+echo "copiado Archivos de html"
+echo
