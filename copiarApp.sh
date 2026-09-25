75846f7d5ff766854950b867010e868e80d53291
MEENN-ACER <ppavez@holograma.cl>
2026-09-25 10:35:10 -0300

    informacion al MOTD

--- copiarApp.sh
+++ copiarApp.sh
@@ -54,10 +54,10 @@ sudo ufw allow samba
 echo
 echo "Activacion de log de UFW de forma correcta..."
 #
-# agregar informacion al pront de inicio de SSH
-sudo cp ./Varios/70-custom-info /etc/update-motd.d/70-custom-informacion
+# agregar informacion al MOTD de inicio de SSH
+sudo cp ./Varios/70-custom-info /etc/update-motd.d/70-custom-info
 sudo cp ./Varios/71-custom-fail2ban /etc/update-motd.d/71-custom-fail2ban
-sudo chmod +x /etc/update-motd.d/70-custom-informacion
+sudo chmod +x /etc/update-motd.d/70-custom-info
 sudo chmod +x /etc/update-motd.d/71-custom-fail2ban
 echo
 echo "Agregado de informacion al pront de inicio de SSH de forma correcta..."
